7a6c6d631c6994e684189ed69550f213ff776efd
Hitesh Jha <hiteshjha@gofynd.com>
2022-11-29 12:28:41 +0530

    FIX: fixed setup.py added pyproject.toml

--- setup.py
+++ setup.py
@@ -9,27 +9,25 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-def get_requirements():
-    """Get Requirement."""
-    with open("requirements.txt") as fp:
-        return [x.strip() for x in fp.read().split("\n") if not x.startswith("#")]
-
-
 setup(
     name="logging_rate_limiter",
-    version="1.0.0",
+    version="0.0.1",
     author="hitesh jha",
     author_email="hitesh4official@gmail.com",
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jha-hitesh/logging-rate-limiter",
-    packages=["logging_rate_limiter"],
+    packages=find_packages(
+        exclude=(
+            "tests"
+        )
+    ),
     license="",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    install_requires=get_requirements(),
+    install_requires=[],
     python_requires=">=3.6",
 )
